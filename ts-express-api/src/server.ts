20e9a761f665edbf265623eaca6aa81254f839bf
vovaushenko <vovaushenko1989@gmail.com>
2021-12-12 16:48:38 -0800

    feat(express): add the scaffold for server

--- ts-express-api/src/server.ts
+++ ts-express-api/src/server.ts
@@ -1,1 +1,12 @@
-console.log('server is running on port 4000');
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req: Request, res: Response) => {
+	return res.send('its working');
+});
+
+app.listen(PORT, () => {
+	console.log(`💻 Server is running on port ${PORT} 💻`);
+});
