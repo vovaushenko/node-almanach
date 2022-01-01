37507d52c16a2aabdc27da7b4e1fa6bd61bb981a
vovaushenko <vovaushenko1989@gmail.com>
2022-01-01 09:14:41 -0800

    feat(nodejs): basic servers

---
+++ 1-http-server/1-basic-classic/server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 8000;
+
+const server = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+server.on('error', (err) => {
+	// EACCESS will be invoked if we don't have permission to open port 8000
+	if (err.code === 'EACCES') {
+		console.log(`No access to port: ${port}`);
+	}
+});
