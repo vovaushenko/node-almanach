407fd491a275b8544ebfedf22fd719dd23d84a2c
vova_ushenko <vova.ushenko@oracle.com>
2022-02-15 08:37:56 -0800

    feat:sqs-sqrs-event-sourcing

--- 4-fs-files/1-working-with-files/2-write-file-sync.js
+++ 4-fs-files/1-working-with-files/2-write-file-sync.js
@@ -5,3 +5,5 @@ const fs = require('fs');
 const data = fs.readFileSync('text.txt', 'utf8');
 const lines = data.split('\n').filter((line) => !!line);
 fs.writeFileSync('1-readFileSync.txt', lines.join(' 🍌 '));
+
+
